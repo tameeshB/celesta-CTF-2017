f6de4a1452e152713731af4276341bfc604d9e1a
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-05 12:49:18 +0530

    q1 done

--- missions/1/index.php
+++ missions/1/index.php
@@ -4,18 +4,27 @@ mustLogin();
 ?>
 <html>
 <head>
-	<title>Shopping Complex</title>
+	<title>File reader app</title>
 	<link rel="stylesheet" type="text/css" href="miss1.css">
 </head>
-<body>
-<div class="cont">
-	<h1>Destroy John</h1><br>
-You have managed to secretly capture code of a local Shopping Complex<br>
-
-Can you delete all the items from their menu.<br>
-<img src="camera.jpg" width=640 height=480>
-<br>
-Link to shopping Complex website is <a href='shopping.php'>here</a>.
-</div>
+<body><center>
+<div class="cont"><h1>Advanced file reading app</h1><br><br><br><br>
+	<?php
+		if(isset($_POST['key'])){
+			if($_POST['key']=="oijw48ywrhw2490q8u2qjl13r8g"){
+				echo "Flag: ".getFlag(1);
+				exit(1);
+			}else{
+				echo "Try again ;)";
+			}
+		}
+	?><br>
+	
+Challenge running at <a href="https://ctf1celesta.herokuapp.com">ctf1celesta.herokuapp.com</a><br><br>
+<form action="" method="POST">
+	<input type="text" name="key"><br><br>
+	<input type="submit" value="Submit">
+</form>
+</div></center>
 </body>
 </html>

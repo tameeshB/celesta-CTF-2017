20a8a63a9ef62678a4663de11cf6423fef48acaf
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-05 07:06:59 +0530

    test

--- timehandler.php
+++ timehandler.php
@@ -1,7 +1,7 @@
 <?php
 	date_default_timezone_set('Asia/Calcutta');
 	
-	$starttime = '10/05/2017 07:00:00 pm';
+	$starttime = '10/05/2017 01:00:00 am';
 	$endtime = '10/06/2017 07:00:00 pm';
 	$now  = date('m/d/Y h:i:s a', time());
 	

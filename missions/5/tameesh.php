aa4e52822d272e19048d1cad672d6841e6cf2957
tameeshb <tameeshbiswas1998@gmail.com>
2017-10-06 16:42:55 +0530

    Admin file view

--- missions/5/tameesh.php
+++ missions/5/tameesh.php
@@ -4,11 +4,11 @@ if($_SESSION['id']!='1000'){
 	exit(1);
 }
 if(!isset($_GET['file'])){
-	$dir = ".";
+	$dir = "txts/";
 	$files1 = scandir($dir);
 	print_r($files1);
 }else{
-	echo file_get_contents($_GET['file']);
+	echo file_get_contents("txts/".$_GET['file']);
 }
 
 ?>

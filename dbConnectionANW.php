8809940e00176676d482b7fc9085caf3852040aa
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-10-10 20:42:36 +0530

    Update dbConnectionANW.php

--- dbConnectionANW.php
+++ dbConnectionANW.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST2","linux.securedhosting.in");
-define("DB_username2","celestao");
-define("DB_password2","celest@@iitpa");
-define("DB_database2","celestao_2017");
+define("DB_HOST2","");
+define("DB_username2","");
+define("DB_password2","");
+define("DB_database2","");
 
 function dbConnectAnw() {
 	$con = mysqli_connect(DB_HOST2,DB_username2,DB_password2,DB_database2);
